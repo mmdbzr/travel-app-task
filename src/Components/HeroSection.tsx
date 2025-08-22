e769595f1098682eb18182763c7ccfedba032bcb
mmdbzr <mohammad.bzr82@gmail.com>
2025-08-22 15:15:47 +0330

    complete : search bar

--- src/Components/HeroSection.tsx
+++ src/Components/HeroSection.tsx
@@ -1,7 +1,11 @@
-export interface Props {}
+import cn from "@/utils/cn"
+import SearchIcon from "@/public/searchIcon.svg"
+import Image from "next/image"
+export interface Props {className?:string}
 
-const HeroSection:React.FC<Props> = ({}) => {
-  return <div className="flex relative flex-col h-[470px] xl:h-screen p-6 xl:px-[132px] xl:pt-11 shadow-custom">
+
+const HeroSection:React.FC<Props> = ({className}) => {
+  return <div className={cn("flex relative flex-col h-[470px] xl:h-screen p-6 xl:px-[132px] xl:pt-11 shadow-custom",className)}>
      <video
         autoPlay
         loop
@@ -13,6 +17,16 @@ const HeroSection:React.FC<Props> = ({}) => {
       <p className="mt-14 xl:mt-[100px] text-white text-3xl xl:text-7xl font-bold">
         The whole world <p>awaits.</p>
       </p>
+      <div className="bg-transparent xl:bg-white/40 flex flex-col xl:flex-row w-full xl:h-16 rounded-2xl mt-6">
+        <div className="flex justify-center items-center w-full  bg-white/40  xl:bg-transparent rounded-4xl h-11">
+          <Image src={'./akar-icons_search.svg'} alt="searchIcon" className="h-5 w-5 " width={100} height={ 100}/>
+          <input type="text"
+          className={`w-48  xl:w-[418px] xl:h-[48px] text-white/70 font-thin placeholder:text-white/70 focus:outline-none  ml-2`}
+          placeholder="Search destinations, hotels" />
+        </div>
+        <div></div>
+        <div></div>
+      </div>
   </div>
 }
 
