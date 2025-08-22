ec371015498b12dc5cd4cd529f4a1bcbe6eb1d86
mmdbzr <mohammad.bzr82@gmail.com>
2025-08-22 14:48:04 +0330

    add : add shadow and bg video to hero section

--- src/Components/HeroSection.tsx
+++ src/Components/HeroSection.tsx
@@ -1,8 +1,19 @@
-
 export interface Props {}
 
 const HeroSection:React.FC<Props> = ({}) => {
-  return <div></div>
+  return <div className="flex relative flex-col h-[470px] xl:h-screen p-6 xl:px-[132px] xl:pt-11 shadow-custom">
+     <video
+        autoPlay
+        loop
+        muted
+        className="absolute top-1/2 left-1/2  w-full h-full max-w-none transform -translate-x-1/2 -translate-y-1/2 object-cover -z-10 border-b-2 border-b-white"
+      >
+        <source src="/bgVideo.mp4" type="video/mp4" />
+      </video>
+      <p className="mt-14 xl:mt-[100px] text-white text-3xl xl:text-7xl font-bold">
+        The whole world <p>awaits.</p>
+      </p>
+  </div>
 }
 
 export default HeroSection
--- src/Components/Shared/Navbar.tsx
+++ src/Components/Shared/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar: React.FC<NavProps> = ({}) => {
   const [activeButton,setActiveButton]=useState<string>("Home");
   const navigationItems=["Home", "Stays", "Flights", "Packages", "Sign Up"];
 
-  return <nav className="flex items-center xl:px-12 py-4 xl:py-6 justify-center xl:justify-between w-full fixed bg-transparent "> 
+  return <nav className="flex items-center xl:px-12 py-4 xl:py-6 justify-center xl:justify-between w-full fixed bg-transparent z-10"> 
     <Image src={menuIcon} width={100} height={100} alt="menuIcon" className="h-6 w-6 fixed left-6 xl:hidden cursor-pointer"/>
     <Image src={Logo} width={100} height={100} alt="Logo" className=" w-14 h-6 xl:h-10 xl:w-20 cursor-pointer "/>
     <div className="hidden xl:flex space-x-8">
